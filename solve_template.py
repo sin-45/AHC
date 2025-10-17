6a142bd6df33c38e0a7a587cfff572945bb3522b
sin-45 <sin45.0403@gmail.com>
2025-10-18 08:27:37 +0900

    solve_template

---
+++ solve_template.py
@@ -0,0 +1,26 @@
+class Solve:
+    def __init__(self):
+        pass
+
+    def solve(self):
+        pass
+    
+    def beam_search(self):
+        pass
+
+    def next_func(self):
+        pass
+    
+    def score_func(self):
+        pass
+
+    def yaki(self):
+        pass
+
+    def output(self):
+        pass
+
+
+if "__main__" == __name__:
+    solve = Solve()
+    solve.solve()
